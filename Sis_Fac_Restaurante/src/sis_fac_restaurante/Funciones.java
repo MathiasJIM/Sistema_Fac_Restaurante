3bce626dac64533ab088b1849f6f4b6997ed129d
Ripichip <josueanticona@gmail.com>
2022-07-22 14:23:21 -0600

    Se empezo a crear la funcion de mostrar pedido en el formulario de mesasa
    Se empezo a crear la funcion de mostrar pedido en el formulario de mesasa

--- Sis_Fac_Restaurante/src/sis_fac_restaurante/Funciones.java
+++ Sis_Fac_Restaurante/src/sis_fac_restaurante/Funciones.java
@@ -9,6 +9,7 @@ public class Funciones {
     
     public int cantidadProductos;
     Menus menu = new Menus();
+    Sis_Fac_Restaurante principal = new Sis_Fac_Restaurante();
     
     public void Mostrar_Tabla(String categoria, javax.swing.JTable tblProductos)
     {
@@ -21,6 +22,7 @@ public class Funciones {
         //Se declara la variable que va a controlar el tamaño de las columnas
         TableColumnModel columnSize = tblProductos.getColumnModel();
         
+        //Variable que se encarga del centrado de las columnas
         DefaultTableCellRenderer centradoColumna = new DefaultTableCellRenderer();
                 
         //Se define la cantidad de filas en 100 para inicializar la tabla
@@ -55,6 +57,59 @@ public class Funciones {
             
             //Condicion para contar la cantidad de productos en la tabla
             if (categoria.equals(menu.menuEntero[j][0]))
+            {
+                //Se suma 1 valor a la cantidad de productos para determinar el tamaño de la tabla al final del ciclo
+                cantidadProductos += 1;
+            }
+        }
+        
+        //Se ajusta el tamaño de la tabla dependiendo del numero de productos en el menu
+        modeloTabla.setRowCount(cantidadProductos);
+    }
+    
+    public void Mostrar_Pedidos(javax.swing.JTable tblPedidos, int numeroMesa)
+    {
+        //Se inicializa cantidad productos en 0 que va a contar los productos que se van a desplegar en el formulario
+        cantidadProductos = 0;
+        
+        //Utilizando DefaultTableModel se genera la variable modeloTabla que va a controlar los tamaños de las tablas
+        DefaultTableModel modeloTabla = (DefaultTableModel) tblPedidos.getModel();
+        
+        //Se declara la variable que va a controlar el tamaño de las columnas
+        TableColumnModel columnSize = tblPedidos.getColumnModel();
+        
+        //Variable que se encarga el centrado de las columnas
+        DefaultTableCellRenderer centradoColumna = new DefaultTableCellRenderer();
+        
+        //Se define la cantidad de filas en 100 para inicializar la tabla
+        modeloTabla.setRowCount(100);
+        
+        //Se define la cantidad de columnas
+        modeloTabla.setColumnCount(2);
+        
+        //Se le da tamaño a las columnas
+        columnSize.getColumn(0).setMaxWidth(30);
+        columnSize.getColumn(0).setPreferredWidth(30);
+        
+        //Se Centran los valores de las columnas en la tabla
+        centradoColumna.setHorizontalAlignment(0);
+        tblPedidos.getColumnModel().getColumn(0).setCellRenderer(centradoColumna);
+        
+        //Ciclo para rellenar la tabla de el formulario con los valores dentro del menu
+        for (int j=0; j<(principal.pedidosMesas.length); j++)
+        {
+            for (int i=0; i<3; i++)
+            {
+                //Si la categoria del producto en el menu coincide con lo que se requiere, se acomoda en la tabla
+                if (numeroMesa == principal.pedidosMesas[j].NumeroMesa)
+                {
+                    //Se agrega 1 por 1 los valores del menu en la tabla, duplicando el menu en la tabla del formulario
+                    tblPedidos.setValueAt(principal.pedidosMesas[j], cantidadProductos, i);
+                }
+            }
+            
+            //Condicion para contar la cantidad de productos en la tabla
+            if (numeroMesa == principal.pedidosMesas[j].NumeroMesa)
             {
                 //Se suma 1 valor a la cantidad de productos para determinar el tamaño de la tabla al final del ciclo
                 cantidadProductos += 1;
